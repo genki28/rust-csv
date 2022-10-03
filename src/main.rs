432cd49884cb780ad8fa0119c7c2b30b2d4f0fbe
genki28 <taiiku39@gmail.com>
2022-10-04 00:34:37 +0900

    print修正

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,7 @@ fn main() -> Result<(), Error> {
   let mut reader = csv::Reader::from_reader(csv.as_bytes());
   for record in reader.records() {
     let record = record?;
-    println!(
-      "In {}, {} built the {} model. It is a {}.",
-      &record[0], &record[1], &record[2], &record[3]
-    );
+    println!("In {}, {}", &record[0], &record[1]);
   }
 
   Ok(())
