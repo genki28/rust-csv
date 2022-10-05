cb2486bb950127d543170b03c91cc8d3e381233d
genki28 <taiiku39@gmail.com>
2022-10-06 01:56:17 +0900

    一旦commit

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,14 @@ fn main() -> Result<(), Error> {
 
   Ok(())
 }
+
+fn csv_to_json() {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
+  file::extension_check(filename);
+  let f = File::open(filename).expect("file not found");
+  let reader = csv::Reader::from_reader(f);
+  let res = parser::parse(reader).unwrap();
+
+  let serialized: String = serde_json::to_string(&res);
+}
